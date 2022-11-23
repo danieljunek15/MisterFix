f285204a1b002921623b9740975c2d77bf455c24
Serhat Ayik <Serhatayik_@hotmail.com>
2022-11-23 16:54:36 +0100

    validator voor elke functie

--- scripts/script_klantenpage_phone.js
+++ scripts/script_klantenpage_phone.js
@@ -13,3 +13,30 @@ phone_input.addEventListener('invalid', () => {
     phone_input.setCustomValidity('Enter phone number in this format: 06-1234568');
   }
 });
+
+
+var form = document.getElementById("form");
+var username = document.getElementById("name");
+var city = document.getElementById("city");
+var street = document.getElementById("street");
+var postal = document.getElementById("postal");
+var email = document.getElementById("mail");
+var password = document.getElementById("password");
+var password2 = document.getElementById("password2");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    
+   const nameValue = name.value
+   const cityValue = city.value
+   const streetValue = street.value
+   const postalValue = postal.value
+   const emailValue = email.value
+   const passwordValue = password.value
+   const password2Value = password2.value
+}
